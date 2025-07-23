948b262015ff774ab61430a801e33e840ed31527
dealbertt <dealbertignacio@gmail.com>
2025-07-23 22:03:26 +0200

    Latest changes

--- init.sh
+++ init.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 initClang(){
-    echo "Initializing Clang project..."
+    echo "Initializing C project..."
     mkdir src
     mkdir include
     touch src/main.c
+    echo "#include <stdio.h>\n\nint main(){\nreturn 0;\n}"
 }
 
-while getopts ":c" opt; do
+while getopts ":c:p:h" opt; do
     case $opt in
-        c)
+        c) #Clang
             initClang
             ;;
-        h)
+        p) #python
+            echo "C++"
+            ;;
+        h) #help
             echo "Help selected"
             echo "Here are all the supported languages at the moment:"
             echo "C/C++"
